9fedee7617226f04bca9a91541b5ace1686b60da
root <root@ip-172-31-3-18.ap-south-1.compute.internal>
2026-04-02 13:03:40 +0000

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: "Hello from Backend!", status: "running" }));
+});
+
+server.listen(3000, () => {
+    console.log('Backend running on port 3000');
+});
